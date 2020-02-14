9d8422e479f78b8361247af985755c5d7799d3d4
zhangyuhong <289590351@qq.com>
2020-02-14 22:00:01 +0800

    功能：学习 运用到的知识点：email的发送以及imtp,mstp,POP3协议 是否完成：NA

---
+++ web_study/src/day05_三层架构和JSP练习/demo05_email_SMTP/MailTest.java
@@ -0,0 +1,29 @@
+package day05_三层架构和JSP练习.demo05_email_SMTP;
+
+/**
+ * @author liuyp
+ * @date 2020/02/13
+ */
+public class MailTest {
+    /**
+     * email的发送以及imtp,mstp,POP3协议
+     *
+     *  SMTP：Simple Mail Transfer Protocol，简单邮件传输协议。发件协议(smtp收件服务器)
+     *  IMAP：Internet Mail Access Protocol，网络邮件访问协议。收件协议，功能更强，
+     *       双向协议：在客户端软件里，对邮件的操作，会同步在服务器里也操作
+     *  POP3：Post Office Protocol - Version 3，邮局协议v3。收件协议
+     *       单向协议：在客户端软件里，对邮件的操作，不会同步在服务器里也操作
+     *
+     * Java发送邮件的使用场景：帐号激活
+     *      有人注册了一个帐号，需要激活
+     *          服务端会给这个人的邮件发送激活邮件，邮件里有激活链接
+     *
+     *      用户登录自己的邮箱，收取激活邮件
+     *          点击其中的激活链接，可以激活帐号
+     *
+     */
+    public static void main(String[] args) throws Exception {
+        //给289590351@qq.com发送邮件
+        MailUtils.sendMail("289590351@qq.com", "hello，老弟");
+    }
+}
---
+++ web_study/src/day05_三层架构和JSP练习/demo05_email_SMTP/MailUtils.java
@@ -0,0 +1,48 @@
+package day05_三层架构和JSP练习.demo05_email_SMTP;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+import java.util.Properties;
+
+/**
+ * 发送邮件工具类
+ */
+public final class MailUtils {
+	private MailUtils(){}
+	/**
+	 * 发送邮件
+	 * 参数一:发送邮件给谁
+	 * 参数二:发送邮件的内容
+	 */
+	public static void sendMail(String toEmail, String emailMsg) throws Exception {
+		// 1.创建一个程序与发件人的 发送邮件服务器会话对象 Session
+		Properties props = new Properties();
+		props.setProperty("mail.transport.protocol", "SMTP");//邮件smtp发送协议
+		props.setProperty("mail.host", "smtp.163.com");//邮件发送服务器的地址，如果使用的是本地安装的易邮，写成：localhost
+ 		Authenticator auth = new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("zhang_kangmu", "zyh0412");
+			}
+		};
+		Session session = Session.getInstance(props, auth);
+
+		//2_创建一封邮件
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress("zhang_kangmu@163.com"));
+		message.setRecipient(RecipientType.TO, new InternetAddress(toEmail));
+		message.setSubject("黑马旅游网帐号激活");
+		message.setContent(emailMsg, "text/html;charset=UTF-8");
+		//3_发送邮件
+		Transport.send(message);
+	}
+}
+
+
+
+
+
+
+
+
